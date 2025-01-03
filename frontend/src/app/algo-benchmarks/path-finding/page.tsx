258e34d6e48b51ca384dfa669decc831697c9e60
= <=>
2025-01-03 21:51:04 +0000

    add path finding page

--- frontend/src/app/algo-benchmarks/path-finding/page.tsx
+++ frontend/src/app/algo-benchmarks/path-finding/page.tsx
@@ -3,7 +3,7 @@ export default function PathFinding() {
         <div>
             {/* Heading */}
             <div>
-                <h1 className="text-3xl mb-8 text-white">Sorting Benchmarks</h1>
+                <h1 className="text-3xl mb-8 text-white">Path Finding Benchmarks</h1>
             </div>
             {/* Content */}
             <div className="flex flex-col gap-4 w-fit py-4">
